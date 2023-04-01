c6c98d0078273eb79b90fdcfc9cf387fe60eb72a
chitt <chittepu.rohithreddy.20cse@bmu.edu.in>
2023-04-01 15:25:16 +0530

    Initial commit
    This is just normal files to clone and start project

---
+++ app/src/main/java/com/example/bmlsalon/Login.java
@@ -0,0 +1,17 @@
+package com.example.bmlsalon;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+
+
+
+    }
+}
---
+++ app/src/main/java/com/example/bmlsalon/Main.java
@@ -0,0 +1,21 @@
+package com.example.bmlsalon;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.Window;
+import android.widget.Toolbar;
+
+public class Main extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+
+
+
+    }
+}
