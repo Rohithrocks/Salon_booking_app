2534ee545c22c0356bf0dabddcd4aec17ab1d6b3
chitt <chittepu.rohithreddy.20cse@bmu.edu.in>
2023-04-12 05:04:50 +0530

    normal changes

--- app/src/main/java/com/example/bmlsalon/Mens_Haircut.java
+++ app/src/main/java/com/example/bmlsalon/Mens_Haircut.java
@@ -3,43 +3,66 @@ package com.example.bmlsalon;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.GridView;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.bmlsalon.databinding.MensHaircutBinding;
 
-import de.hdodenhof.circleimageview.CircleImageView;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+
 
 public class Mens_Haircut extends AppCompatActivity {
         private MensHaircutBinding binding;
 
+        Button backtomain;
+
+    private ArrayList<String> availableSlots;
+    private GridView gridView;
+    private TextView totalPriceTextView;
+    private Button checkoutButton;
+
         @Override
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
-
-            // Initialize the binding object
             binding = MensHaircutBinding.inflate(getLayoutInflater());
             View view = binding.getRoot();
             setContentView(view);
 
-      ImageButton myButton = binding.back;
 
-        myButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                switch (v.getId()) {
-                    case R.id.back:
+            backtomain = binding.back; // Assuming binding is an instance of the appropriate binding class for the layout
 
-                        Intent intent1 = new Intent(Mens_Haircut.this, HomeFragment.class);
-                        startActivity(intent1);
-                        overridePendingTransition(R.anim.slide_in, R.anim.slide_out);
-                        break;
+            backtomain.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    switch (v.getId()) {
+                        case R.id.back:
+                            // Create a new instance of the HomeFragment
+                            HomeFragment homeFragment = new HomeFragment();
 
-                    default:
-                        break;
+                            // Get the FragmentManager and start a FragmentTransaction
+                            FragmentManager fragmentManager = getSupportFragmentManager();
+                            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+                            // Replace the current fragment (if any) with the HomeFragment
+                            fragmentTransaction.replace(R.id.back, homeFragment); // R.id.container is the ID of the container in which the fragment will be displayed
+                            fragmentTransaction.addToBackStack(null); // Add the transaction to the back stack, so the user can navigate back
+                            fragmentTransaction.commit(); // Commit the transaction
+                            break;
                 }
             }
         });
